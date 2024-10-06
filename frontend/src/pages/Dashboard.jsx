bfe666cb375a941e11d811ca7dbfbd903ad4d28d
Ankur Das <132288002+Ankur6580@users.noreply.github.com>
2024-10-06 16:34:46 +0530

    added a dashboard placeholder image

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { getDashboard } from '../api/dashboard/dashboard';
+import dashboard from '../assets/dashboard.png';
 import '../styles/dashboard.css';
 
 const Dashboard = () => {
@@ -31,9 +32,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-heading">Welcome to Your Dashboard</h1>
-      <p className="dashboard-paragraph">
-        I sincerely hope that I have successfully met your expectations with this assignment. It has been a rewarding challenge to demonstrate my skills and creativity through this project. Thank you for the opportunity, and I look forward to your feedback.
-      </p>
+      <div className="dashboard-image"><img src={dashboard}/></div>
     </div>
   );
 };
